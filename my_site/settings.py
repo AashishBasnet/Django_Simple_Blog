46a2e3bddbd6e47cfb1128efad7e580c47d468b5
AashishBasnet <basnetashish756@gmail.com>
2024-10-19 13:49:56 +0545

    database addes

--- my_site/settings.py
+++ my_site/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # Allowed hosts
 ALLOWED_HOSTS = [
-    '.vercel.app' , '.now.sh'
+    '.vercel.app' , '.now.sh', '127.0.0.1', 'localhost'
 ]
 
 
@@ -80,13 +80,18 @@ WSGI_APPLICATION = "my_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER' : 'postgres',
+        'PASSWORD' : 'QJndGnZfbKCdjyzkfJXwhHkaDjqeBcah',
+        'HOST' : 'postgres.railway.internal',
+        'PORT' : '5432',
+    }
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
