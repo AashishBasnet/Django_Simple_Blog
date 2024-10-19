6c42a54e08abd396a525b4bf09be8db28bd12802
AashishBasnet <basnetashish756@gmail.com>
2024-10-19 14:39:23 +0545

    postgre database addition

--- my_site/settings.py
+++ my_site/settings.py
@@ -80,7 +80,12 @@ WSGI_APPLICATION = "my_site.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# } 
 
 DATABASES = {
     'default': {
@@ -88,10 +93,11 @@ DATABASES = {
         'NAME': 'railway',
         'USER' : 'postgres',
         'PASSWORD' : 'QJndGnZfbKCdjyzkfJXwhHkaDjqeBcah',
-        'HOST' : 'postgres.railway.internal',
-        'PORT' : '5432',
+        'HOST' : 'junction.proxy.rlwy.net',
+        'PORT' : '20894',
     }
 } 
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
